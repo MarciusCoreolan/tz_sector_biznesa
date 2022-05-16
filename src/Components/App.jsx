b29cbedebc8447fa1729808fac28b76d8840e462
FeNRiR <germes955@gamail.com>
2022-05-16 21:41:09 +0300

    fix pagination finishing project

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,19 +1,18 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 
-import "../Styles/index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../Styles/index.scss";
 
 import { getPosts } from "../Redux/actions";
-
 import Header from "./Header/Header";
 import Table from "./Table/Table";
 import Footer from "./Footer/Footer";
-import {Navigate, Route, Routes, useLocation} from "react-router-dom";
 
-function App(props) {
+function App() {
   const dispatch = useDispatch();
-  const local = useLocation().pathname[1]
+  const local = useLocation().pathname.substr(1);
 
   useEffect(() => {
     dispatch(getPosts(local));
@@ -22,10 +21,10 @@ function App(props) {
   return (
     <div>
       <Header />
-        <Routes>
-            <Route path={`/${local}`} element={<Table />}/>
-            <Route path={"*"} element={<Navigate to={"/1"} replace />} />
-        </Routes>
+      <Routes>
+        <Route path={`/${local}`} element={<Table />} />
+        <Route path={"*"} element={<Navigate to={"/1"} replace />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -2,24 +2,16 @@ const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
 export const GET_POSTS_START = "get/posts/start";
 export const GET_POSTS_SUCCESSES = "get/posts/successes";
 export const GET_POSTS_ERROR = "get/posts/error";
-export const SELECT_PAGE = "select/page";
-export const NEXT_PAGE = "next/page";
-export const PREV_PAGE = "prev/page";
 export const SEARCH_ON_TABLE = "search/on/table";
 
 export const getPosts = (page) => {
-  console.log(page)
   return async (dispatch) => {
     try {
       dispatch({
         type: GET_POSTS_START,
       });
-
       const res = await fetch(BASE_URL + `?_limit=10&_page=${page}`);
-
       const json = await res.json();
-
-      console.log(json)
 
       dispatch({
         type: GET_POSTS_SUCCESSES,
